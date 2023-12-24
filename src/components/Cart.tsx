c3628aead419c68ef17905537f7f8de75e30b51e
ozasatyam <satyamoza@outlook.com>
2023-12-23 23:25:56 -0700

    cart view at nev bar

---
+++ src/components/Cart.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import {
+    Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { ShoppingCart } from "lucide-react";
+
+const Cart = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="group -m-2 flex items-center p-2">
+        <ShoppingCart
+          aria-hidden="true"
+          className="h-6 w-6 flex-shrink text-gray-400 group-hover:text-gray-500"
+        />
+        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+          0
+        </span>
+      </SheetTrigger>
+      <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+        <SheetHeader className="spacey-2.5 pr-6">
+          <SheetTitle> cart(0)</SheetTitle>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default Cart;
