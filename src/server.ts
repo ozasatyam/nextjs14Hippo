911355502893ac06552c2a63a07e45724fb985fe
ozasatyam <satyamoza@outlook.com>
2023-12-25 23:51:13 -0700

    self hosting server for next js

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
-// import express from "express"
-// const app = express()
-// const PORT = Number(process.env.PORT) || 300
+import express from "express"
+import { getPayloadClient } from "./get-payload"
+import { nextApp, nextHandler } from "./next-utils"
+const app = express()
+const PORT = Number(process.env.PORT) || 3000
 
-// const start = async () => {
-// const payload = await getPayloadClient()
-// }
+const start = async () => {
+    const payload = await getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async (cms) => {
+                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
+            }
+        }
+    })
+    app.use((req, res) => nextHandler(req, res))
+    nextApp.prepare().then(() => {
+        payload.logger.info("Next js Started")
+        app.listen(PORT, async () => {
+            payload.logger.info(`Next js APp URL ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+        })
+    })
+}
+start()
