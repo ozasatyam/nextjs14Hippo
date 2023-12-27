9870f040cf065903ecdf230b7f8029315cd017bb
ozasatyam <satyamoza@outlook.com>
2023-12-26 23:39:36 -0700

    undo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
-console.log("just dont wanna miss the git")
 const perks = [
   {
     name: "Instant Delivery",
