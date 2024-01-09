fd5dca6a94572c955246cafcc2261bb4b1280b26
ozasatyam <satyamoza@outlook.com>
2024-01-08 23:00:18 -0700

    product displaying

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { TqueryValidator } from "@/lib/validators/query-validator";
+import { Product } from "@/payload-types";
 import { trpc } from "@/trpc/client";
 import Link from "next/link";
 import React from "react";
+import ProductListing from "./ProductLising";
 interface ProductReelProps {
   title: string;
   subtitle?: string;
@@ -14,17 +16,23 @@ function ProductReel(props: ProductReelProps) {
 
   const FALLBACK_LIMIT = 4;
 
-  const { data } = trpc.getInfiniteProducts.useInfiniteQuery(
-    {
-      limit: query.limit ?? FALLBACK_LIMIT,
-      query,
-    },
-    {
-      getNextPageParam: (lastpage) => lastpage.nextPage,
-    }
-  );
-  console.log("first", data);
-
+  const { data: queryResult, isLoading } =
+    trpc.getInfiniteProducts.useInfiniteQuery(
+      {
+        limit: query.limit ?? FALLBACK_LIMIT,
+        query,
+      },
+      {
+        getNextPageParam: (lastpage) => lastpage.nextPage,
+      }
+    );
+  const products = queryResult?.pages.flatMap((page) => page.items);
+  let map: (Product | null)[] = [];
+  if (products && products.length) {
+    map = products;
+  } else if (isLoading) {
+    map = new Array<null>(query.limit ?? FALLBACK_LIMIT).fill(null);
+  }
   return (
     <section className="py-12">
       <div className="md:flex md:items-center md:justify-between mb-4">
@@ -49,7 +57,11 @@ function ProductReel(props: ProductReelProps) {
       </div>
       <div className="relative">
         <div className="mt-6 flex items-center w-full">
-          <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10"></div>
+          <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10">
+            {map.map((product, i) => (
+              <ProductListing index={i} product={product} key={product?.id} />
+            ))}
+          </div>
         </div>
       </div>
     </section>
