f57535850131947958b34b7e41ea0ffa5f4a2926
ozasatyam <satyamoza@outlook.com>
2024-01-11 23:05:22 -0700

    product detail page and prodt list slider like air bnb

---
+++ src/app/product/[productId]/page.tsx
@@ -0,0 +1,103 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { getPayloadClient } from "@/get-payload";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import React from "react";
+interface PageProps {
+  params: {
+    productId: string;
+  };
+}
+const BREADCRUMBS = [
+  {
+    id: 1,
+    name: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    name: "Product",
+    href: "/producs",
+  },
+];
+async function page({ params }: PageProps) {
+  const { productId } = params;
+  const payload = await getPayloadClient();
+  const { docs: products } = await payload.find({
+    collection: "products",
+    limit: 1,
+    where: {
+      id: {
+        equals: productId,
+      },
+      approvedForSale: {
+        equals: "approved",
+      },
+    },
+  });
+
+  const [product] = products;
+
+  if (!product) return notFound();
+
+  const label = PRODUCT_CATEGORIES.find(
+    ({ value }) => value === product.category
+  )?.label;
+
+  return (
+    <MaxWidthWrapper className="bg-white">
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+          {/* product detai; */}
+          <div className="lg:max-w-lg lg:self-end">
+            <ol className="flex items-center space-x-2">
+              {BREADCRUMBS.map((breadcrumbs, i) => {
+                return (
+                  <li key={breadcrumbs.href}>
+                    <div className="flex items-center text-sm">
+                      <Link
+                        href={breadcrumbs.href}
+                        className="font-medium text-sm text-muted-foreground hover:text-gray-900"
+                      >
+                        {breadcrumbs.name}
+                      </Link>
+                      {i !== BREADCRUMBS.length - 1 ? (
+                        <svg
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true"
+                          className="ml-2 h-5 w-5 flex-shrink-0 text-gray-300"
+                        >
+                          <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                        </svg>
+                      ) : null}
+                    </div>
+                  </li>
+                );
+              })}
+            </ol>
+            <div className="mt-4">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                {product.name}
+              </h1>
+            </div>
+            <section className="mt-4">
+              <div className="flex items-center">
+                <p className="font-medium text-gray-900">
+                  {formatPrice(product.price)}
+                </p>
+                <div className="ml-4 border-l text-muted-foreground border-gray-300 pl-4">
+                  {label}
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  );
+}
+
+export default page;
--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -2,33 +2,90 @@ import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-
+import { useEffect, useState } from "react";
+import type SwiperType from "swiper";
+import { Pagination } from "swiper/modules";
+import { cn } from "@/lib/utils";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 interface ImageSliderProps {
   urls: string[];
 }
 const ImageSlider = ({ urls }: ImageSliderProps) => {
+  const [swiper, setSwiper] = useState<null | SwiperType>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const [slideConfig, setSlideConfig] = useState({
+    isBeginning: true,
+    isEnd: activeIndex === (urls.length ?? 0) - 1,
+  });
+  useEffect(() => {
+    swiper?.on("slideChange", ({ activeIndex }) => {
+      setActiveIndex(activeIndex);
+      setSlideConfig({
+        isBeginning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1,
+      });
+    });
+  }, [swiper, urls]);
   const activeStyle =
     "active:scale-[0.97] gird opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-sequare h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
   const inActiveStyle = "hidden text-gray-400";
   return (
     <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
       <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
-        <button></button>
-        <button></button>
-        <Swiper className="h-full w-full ">
-          {urls.map((url, i) => (
-            <SwiperSlide key={i} className="-z-10 relative h-full w-full">
-              <Image
-                fill
-                loading="eager"
-                className="-z-10 h-full w-full object-cover object-center"
-                src={url}
-                alt="product image"
-              ></Image>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slideNext();
+          }}
+          className={cn(activeStyle, "right-3 transition", {
+            [inActiveStyle]: slideConfig.isEnd,
+            "hover:bg-primary-300 text-primary-800 opacity-100":
+              !slideConfig.isEnd,
+          })}
+          aria-label="next image"
+        >
+          <ChevronRight className="h-4 2-4 text-zinc-700"></ChevronRight>
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slidePrev();
+          }}
+          className={cn(activeStyle, "left-3 transition", {
+            [inActiveStyle]: slideConfig.isBeginning,
+            "hover:bg-primary-300 text-primary-800 opacity-100":
+              !slideConfig.isBeginning,
+          })}
+          aria-label="previous image"
+        >
+          <ChevronLeft className="h-4 2-4 text-zinc-700"></ChevronLeft>
+        </button>
       </div>
+      <Swiper
+        pagination={{
+          renderBullet: (_, className) => {
+            return `<span class="rounded-full transition ${className}"</span>`;
+          },
+        }}
+        onSwiper={(swiper) => setSwiper(swiper)}
+        spaceBetween={50}
+        slidesPerView={1}
+        modules={[Pagination]}
+        className="h-full w-full "
+      >
+        {urls.map((url, i) => (
+          <SwiperSlide key={i} className="-z-10 relative h-full w-full">
+            <Image
+              fill
+              loading="eager"
+              className="-z-10 h-full w-full object-cover object-center"
+              src={url}
+              alt="product image"
+            ></Image>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
