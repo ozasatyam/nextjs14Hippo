d74a38e587f11a1c31013b005fe55af5b7ee8335
ozasatyam <satyamoza@outlook.com>
2024-01-16 23:15:44 -0700

    cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,11 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { PRODUCT_CATEGORIES } from "@/config";
 import { useCart } from "@/hooks/use-cart";
-import { cn } from "@/lib/utils";
+import { cn, formatPrice } from "@/lib/utils";
+import { Check, X } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { boolean } from "zod";
 
@@ -55,7 +58,7 @@ function page() {
             >
               {isMounted &&
                 items.map(({ product }) => {
-                  const category = PRODUCT_CATEGORIES.find(
+                  const label = PRODUCT_CATEGORIES.find(
                     (c) => c.value === product.category
                   )?.label;
                   const { image } = product.images[0];
@@ -73,11 +76,54 @@ function page() {
                           ) : null}
                         </div>
                       </div>
+                      <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
+                        <div className="relative pr-9 sm:grid sm:pr-0 sm:gap-x-6 sm:grid-cols-2 ">
+                          <div>
+                            <div className="flex justify-between">
+                              <h3 className="text-sm">
+                                <Link
+                                  href={`/product/${product.id}`}
+                                  className="font-medium text-gray-700 hover:text-gray-800"
+                                >
+                                  {product.name}
+                                </Link>
+                              </h3>
+                            </div>
+                            <div className="mt-1 flex text-sm">
+                              <p className="text-muted-foreground">
+                                Category:{label}
+                              </p>
+                            </div>
+                            <p className="mt-1 text-sm font-medium text-gray-900">
+                              {formatPrice(product.price)}
+                            </p>
+                          </div>
+                          <div className="mt-4 sm:mt-0 sm:pr-9 w-20">
+                            <div className="absolut right-0 top-0">
+                              <Button
+                                aria-label="remove Product"
+                                variant={"ghost"}
+                                onClick={() => removeItem(product.id)}
+                              >
+                                <X className="h-5 w-5" aria-hidden="true"></X>
+                              </Button>
+                            </div>
+                          </div>
+                        </div>
+                        <p className="mt-4 flex space-x-2 test-sm text-gray-700">
+                          <Check className="h-5 w-5 flex-shrink-0 text-green-500"></Check>
+                          <span>Eligible for instant delivery</span>
+                        </p>
+                      </div>
                     </li>
                   );
                 })}
             </ul>
           </div>
+
+          <section className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:mt-0 lg:col-span-5 lg:p-8 ">
+            <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
+          </section>
         </div>
       </div>
     </div>
