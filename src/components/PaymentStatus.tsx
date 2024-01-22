641d8c227bc8eb572c755c2f09cc8096d3c91c09
ozasatyam <satyamoza@outlook.com>
2024-01-21 22:53:37 -0700

    payment status chage

--- src/components/PaymentStatus.tsx
+++ src/components/PaymentStatus.tsx
@@ -1,11 +1,26 @@
 "user client";
-import React from "react";
+import { trpc } from "@/trpc/client";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 interface PaymentStatusProps {
   orderEmail: string;
   orderId: string;
   isPaid: boolean;
 }
 function PaymentStatus({ orderEmail, orderId, isPaid }: PaymentStatusProps) {
+  const router = useRouter();
+  const { data } = trpc.payment.pollOrderStatus.useQuery(
+    { orderId },
+    {
+      enabled: isPaid === false,
+      refetchInterval: (data) => (data?.isPaid ? false : 1000),
+    }
+  );
+
+  useEffect(() => {
+    if (data?.isPaid) router.refresh();
+  }, [data?.isPaid, router]);
+
   return (
     <div className="mt-16 grid grid-cols-2 gap-x-4 text-sm text-gray-600">
       <div>
--- src/trpc/payment-rounter.ts
+++ src/trpc/payment-rounter.ts
@@ -68,5 +68,23 @@ export const paylmentRoutner = router({
                 console.log(err)
                 return { url: null }
             }
+        }),
+
+    pollOrderStatus: privateProcedure.input(z.object({ orderId: z.string() })).query(async ({ input }) => {
+        const { orderId } = input
+        const payload = await getPayloadClient()
+        const { docs: orders } = await payload.find({
+            collection: "orders",
+            where: {
+                id: {
+                    equals: orderId
+                }
+            }
         })
+        if (!orders.length) {
+            throw new TRPCError({ code: "NOT_FOUND" })
+        }
+        const [order] = orders
+        return { isPaid: order._isPaid }
+    })
 })
